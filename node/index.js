0fa9f813c3e963015ae0a38a2c47c747dcf53a25
Afthercaf <josuemoisesuicab@gmail.com>
2024-07-26 01:17:47 -0600

    Descripción de tus cambios

--- node/index.js
+++ node/index.js
@@ -5,3 +5,16 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+app.get("/", (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+});
