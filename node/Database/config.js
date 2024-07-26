5cc81773634adb4ef84b5465332ad378775350dc
Afthercaf <josuemoisesuicab@gmail.com>
2024-07-26 12:36:10 -0600

    Descripción de tus cambios

--- node/Database/config.js
+++ node/Database/config.js
@@ -1,18 +1,17 @@
 import { config } from "dotenv";
 config();
 
-export const database = {
-  
-  host: "byvpcdvsjnucwwvbsx2j-mysql.services.clever-cloud.com",
-  user: "uftqeqin9n8keva6",
-  password: "6WKCHe7CyW7G9K9DcyYE",
-  database: "byvpcdvsjnucwwvbsx2j",
-  createDatabaseTable: true,
-  connectionLimit: 10,
-  waitForConnections: true,
-  connectTimeout: 20000, // Aumenta el tiempo de espera a 20 segundos
 
+export const database = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 10,
+    waitForConnections: true,
+    connectTimeout: 20000, // Aumenta el tiempo de espera a 20 segundos
 };
+
 
 
 export const PORT =  4000;
--- node/lib/helpers.js
+++ node/lib/helpers.js
@@ -13,18 +13,30 @@ export async function createAccessToken(payload) {
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
+if (!ACCESS_TOKEN_SECRET) {
+  console.error('ACCESS_TOKEN_SECRET no está definido en las variables de entorno');
+  process.exit(1); // Salir del proceso si la variable no está definida
+}
+
 export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+  if (token == null) {
+    console.error('Token no proporcionado en la cabecera de autorización');
+    return res.sendStatus(401); // No autorizado
+  }
+
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.error('Error al verificar el token:', err);
+      return res.sendStatus(403); // Prohibido
+    }
+
     req.user = user;
     next();
   });
 };
-
 
 
 import bcrypt from "bcryptjs";
